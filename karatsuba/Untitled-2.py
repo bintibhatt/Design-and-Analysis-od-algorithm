d2f2070ad5e01e532fbcb3cf5e0778dec056dbe7
Binti Bhatt <bintijbhatt@gmail.com>
2022-10-05 10:49:13 +0530

    Delete Untitled-2.py

--- karatsuba/Untitled-2.py
+++
@@ -1,8 +0,0 @@
-
-    for i in range(len(data)):
-        for j in range(len(data), 0 , -1):
-            if data[j] < data[j-1]:
-                t = data[j]
-                data[j] = data[j-1]
-                data[j-1] = data[j]
-print(data)
